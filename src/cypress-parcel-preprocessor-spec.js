2fa0e6abfc441c199a823b37fb69ad957fa2d5d8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-03 15:55:17 -0500

    chore: initial

---
+++ src/cypress-parcel-preprocessor-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const cypressParcelPreprocessor = require('.')
+
+describe('cypress-parcel-preprocessor', () => {
+  it('write this test', () => {
+    console.assert(cypressParcelPreprocessor, 'should export something')
+  })
+})
