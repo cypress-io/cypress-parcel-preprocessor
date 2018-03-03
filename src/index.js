a536025ccfb35c9557f9ee1a87b8fae759c4f318
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-03 16:53:55 -0500

    feat: first pass

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 // @ts-check
 const ParcelBundler = require('parcel-bundler')
 
-const onFile = (file) => {
-  console.log('file:preprocessor %s', file.filePath)
+const bundlers = {}
 
-  const options = {watch: false}
-  const bundler = new ParcelBundler(file.filePath, options)
+const bundleOnce = filePath => {
+  const bundler = new ParcelBundler(filePath, { watch: false })
+  return bundler.bundle().then(b => b.name)
+}
 
-  return bundler.bundle().then(b => {
-    console.log('finished with bundle %s', b.name)
-    return b.name
+const onFile = file => {
+  const { filePath, shouldWatch } = file
+  console.log('file:preprocessor %s', filePath)
+
+  if (bundlers[filePath]) {
+    console.log('file already bundled %s', filePath)
+    return bundlers[filePath]
+  }
+
+  if (!shouldWatch) {
+    return bundleOnce(filePath)
+  }
+
+  const options = {
+    watch: shouldWatch,
+    // make output simpler and avoid possible conflicts with Cypress
+    // by NOT having hot module reloading
+    hmr: false
+  }
+  const bundler = new ParcelBundler(filePath, options)
+
+  bundlers[filePath] = new Promise((resolve, reject) => {
+    bundler.on('bundled', b => {
+      console.log('bundled %s', filePath)
+      console.log('into %s', b.name)
+      file.emit('rerun')
+      resolve(b.name)
+    })
+    bundler.bundle().catch(reject)
   })
+
+  // TODO: add cleanup when watching the file is no longer necessary
+  return bundlers[filePath]
 }
 
 module.exports = onFile
-
-// (file) => {
-//   console.log('file:preprocessor %s should watch? %j', file.filePath, file.shouldWatch)
-//   // TODO handle files that should not be watched
-
-//   if (bundlers[file.filePath]) {
-//     return bundlers[file.filePath]
-//   }
-
-//   const options = {watch: file.shouldWatch}
-//   const bundler = new ParcelBundler(file.filePath, options)
-
-//   bundlers[file.filePath] = new Promise((resolve, reject) => {
-//     bundler.on('bundled', (b) => {
-//       file.emit('rerun')
-//     })
-//     bundler.bundle().then(b => {
-//       resolve(b.name)
-//     })
-//   })
-
-//   return bundlers[file.filePath]
-// }
--- src/spec.js
+++ src/spec.js
@@ -1,4 +1,4 @@
-//@ts-check
+// @ts-check
 
 const la = require('lazy-ass')
 const is = require('check-more-types')
@@ -10,11 +10,17 @@ describe('cypress-parcel-preprocessor', () => {
   const cyParcel = require('./index')
 
   it('bundles given file', () => {
-    const specFilename = join(__dirname, '..', 'cypress', 'integration', 'spec.js')
-    return cyParcel({filePath: specFilename})
-      .then(filename => {
-        la(is.unemptyString(filename), 'expected bundled filename', filename)
-        la(exists(filename), 'cannot find output bundle', filename)
-      })
+    const specFilename = join(
+      __dirname,
+      '..',
+      'cypress',
+      'integration',
+      'spec.js'
+    )
+    const emit = () => {}
+    return cyParcel({ filePath: specFilename, emit }).then(filename => {
+      la(is.unemptyString(filename), 'expected bundled filename', filename)
+      la(exists(filename), 'cannot find output bundle', filename)
+    })
   })
 })
